f38c3a934b985d319beea32b8543883fc3a8d318
AlexanderPhan <42187090+AlexanderPhan@users.noreply.github.com>
2018-10-30 18:24:15 -0500

    index.js

---
+++ Word.js
@@ -0,0 +1,40 @@
+var Letter = require("./Letter");
+
+
+function Word(words) {
+
+    this.words = words;
+    this.newLetters = [];
+
+
+    this.lettersMade = function () {
+        var wordArray = this.words.split("");
+        for (let x = 0; x < wordArray.length; x++) {
+
+            var newLetter = new Letter(wordArray[x]);
+            this.letters.push(newLetter);
+        }
+    };
+
+    this.userGuess = function (guess) {
+        this.letters.forEach(letter => {
+            letter.checkGuess(guess);
+
+        });
+
+    };
+
+    this.game = function () {
+        var printedWord = "";
+        this.letters.forEach(letter => {
+            printedWord += letter.showResult() + "";
+
+        });
+        return printedWord;
+    }
+
+};
+
+
+
+module.exports = Word;
